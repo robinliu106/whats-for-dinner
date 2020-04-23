bdd404c059f34afb7fd5b64ba27f4a904d1ab704
Robin Liu <robinliu106@gmail.com>
2020-04-23 18:41:21 -0400

    add counter

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 console.log("app.js is ruunning");
 
-var app = {
+const app = {
     title: "Indecision App",
     subtitle: "Put your life in the hands of a computer",
     options: ["Option One", "Option Two"],
 };
 
-var template = (
+const template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
@@ -19,28 +19,39 @@ var template = (
     </div>
 );
 
-var user = {
-    name: "Robin",
-    age: 25,
-    location: "Davis Square",
+let count = 0;
+
+const addOne = () => {
+    count++;
+    renderCounterApp();
 };
 
-function getLocation(location) {
-    if (location) {
-        return <p>Location: {location}</p>;
-    }
-}
+const minusOne = () => {
+    count--;
+    renderCounterApp();
+};
 
-var templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : "Anonymous"}</h1>
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+};
 
-        {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
-        {getLocation(user.location)}
-    </div>
-);
+const appRoot = document.getElementById("app");
 
-var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    );
+    ReactDOM.render(templateTwo, appRoot);
+};
 
+renderCounterApp();
+
+//Run 2 terminals
 //babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+//live-server public
---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,11 @@
+var nameVar = "Robin";
+var nameVar = "Maxine";
+console.log("nameVar", nameVar);
+
+let nameLet = "Jen";
+// let nameLet = "Joe";
+console.log("nameLet", nameLet);
+
+const nameConst = "Frank";
+
+console.log("nameConst", nameConst);
