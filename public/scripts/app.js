7691e657d36e364d2954ac380876e910112a5738
Robin Liu <robinliu106@gmail.com>
2020-04-23 17:32:29 -0400

    finished section 12

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,21 +2,62 @@
 
 console.log("app.js is ruunning");
 
-//JSX - JavaScript XML
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer",
+    options: ["Option One", "Option Two"]
+};
+
 var template = React.createElement(
     "div",
     null,
     React.createElement(
         "h1",
         null,
-        "Indecision App"
+        app.title
+    ),
+    app.subtitle && React.createElement(
+        "p",
+        null,
+        app.subtitle
     ),
     React.createElement(
         "p",
         null,
-        "This is some info"
+        app.options.length > 0 ? "Here are your options" : "No Options"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item Two"
+        )
     )
 );
+
+var user = {
+    name: "Robin",
+    age: 25,
+    location: "Davis Square"
+};
+
+function getLocation(location) {
+    if (location) {
+        return React.createElement(
+            "p",
+            null,
+            "Location: ",
+            location
+        );
+    }
+}
 
 var templateTwo = React.createElement(
     "div",
@@ -24,21 +65,18 @@ var templateTwo = React.createElement(
     React.createElement(
         "h1",
         null,
-        "Robin Liu"
+        user.name ? user.name : "Anonymous"
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         "p",
         null,
-        "Age: 24"
+        "Age: ",
+        user.age
     ),
-    React.createElement(
-        "p",
-        null,
-        "Location: Boston"
-    )
+    getLocation(user.location)
 );
 
 var appRoot = document.getElementById("app");
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
 
 //babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
--- src/app.js
+++ src/app.js
@@ -1,22 +1,46 @@
 console.log("app.js is ruunning");
 
-//JSX - JavaScript XML
+var app = {
+    title: "Indecision App",
+    subtitle: "Put your life in the hands of a computer",
+    options: ["Option One", "Option Two"],
+};
+
 var template = (
     <div>
-        <h1>Indecision App</h1>
-        <p>This is some info</p>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+
+        <p>{app.options.length > 0 ? "Here are your options" : "No Options"}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item Two</li>
+        </ol>
     </div>
 );
 
+var user = {
+    name: "Robin",
+    age: 25,
+    location: "Davis Square",
+};
+
+function getLocation(location) {
+    if (location) {
+        return <p>Location: {location}</p>;
+    }
+}
+
 var templateTwo = (
     <div>
-        <h1>Robin Liu</h1>
-        <p>Age: 24</p>
-        <p>Location: Boston</p>
+        <h1>{user.name ? user.name : "Anonymous"}</h1>
+
+        {user.age && user.age >= 18 && <p>Age: {user.age}</p>}
+        {getLocation(user.location)}
     </div>
 );
 
 var appRoot = document.getElementById("app");
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
 
 //babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
