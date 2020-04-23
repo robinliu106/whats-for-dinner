a7aa47e483680b9e6f4b0bc6ba500d57ae0eaa14
Robin Liu <robinliu106@gmail.com>
2020-04-22 23:52:45 -0400

    initial

---
+++ public/scripts/app.js
@@ -0,0 +1,7 @@
+console.log("app.js is ruunning");
+
+//JSX - JavaScript XML
+var template = <p>This is JSX from app.js</p>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
