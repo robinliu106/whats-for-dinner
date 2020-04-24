14e7a282f4164179c8bc7eb267acbc76c61567a1
Robin Liu <robinliu106@gmail.com>
2020-04-24 14:57:48 -0400

    extended Persons class

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -7,10 +7,46 @@ class Person {
         return `Hi I am ${this.name}!`;
     }
     getDescription() {
-        return `${this.name} is ${this.age} year(s) old`;
+        return `${this.name} is ${this.age} year(s) old.`;
     }
 }
 
-const me = new Person("Robin Liu");
-const you = new Person();
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+
+    getDescription() {
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}`;
+        }
+        return description;
+    }
+}
+
+class Traveller extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if (this.hasHomeLocation()) {
+            greeting += ` I am visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+}
+
+const me = new Traveller("Robin Liu", 24, "Hong Kong");
+const you = new Traveller();
+console.log(me.getGreeting());
+console.log(you.getGreeting());
