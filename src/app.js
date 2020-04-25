50c7ea6e7bcbc0882e437a24de27d5238c095f53
Robin Liu <robinliu106@gmail.com>
2020-04-25 12:27:20 -0400

    converted Action class to stateless function

--- src/app.js
+++ src/app.js
@@ -65,20 +65,30 @@ class Header extends React.Component {
     }
 }
 
-class Action extends React.Component {
-    render() {
-        return (
-            <div>
-                <button
-                    onClick={this.props.handlePick}
-                    disabled={!this.props.hasOptions}
-                >
-                    What should I do?
-                </button>
-            </div>
-        );
-    }
-}
+const Action = (props) => {
+    return (
+        <div>
+            <button onClick={props.handlePick} disabled={!props.hasOptions}>
+                What should I do?
+            </button>
+        </div>
+    );
+};
+
+// class Action extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <button
+//                     onClick={this.props.handlePick}
+//                     disabled={!this.props.hasOptions}
+//                 >
+//                     What should I do?
+//                 </button>
+//             </div>
+//         );
+//     }
+// }
 
 class Options extends React.Component {
     render() {
@@ -134,4 +144,14 @@ class AddOption extends React.Component {
     }
 }
 
+// const User = (props) => {
+//     return (
+//         <div>
+//             <p>Name: {props.name}</p>
+//             <p>Age: {props.age}</p>
+//         </div>
+//     );
+// };
+
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
+// ReactDOM.render(<User name="Robin" age={24} />, document.getElementById("app"));
