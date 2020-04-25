82340d7c8fb9c46eb68abebed504f360aba01919
Robin Liu <robinliu106@gmail.com>
2020-04-25 16:59:43 -0400

    added localstorage to counter example

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,9 +5,24 @@ class Counter extends React.Component {
         this.handleMinusOne = this.handleMinusOne.bind(this);
         this.handleReset = this.handleReset.bind(this);
         this.state = {
-            count: props.count,
+            count: 0,
         };
     }
+    //life cycle methods
+    componentDidMount() {
+        const stringCount = localStorage.getItem("count");
+        const count = parseInt(stringCount, 10);
+
+        if (!isNaN(count)) {
+            this.setState(() => ({ count }));
+        }
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.count !== this.state.count) {
+            localStorage.setItem("count", this.state.count);
+        }
+    }
+
     handleAddOne() {
         this.setState((prevState) => {
             return {
@@ -41,11 +56,7 @@ class Counter extends React.Component {
     }
 }
 
-Counter.defaultProps = {
-    count: 0,
-};
-
-ReactDOM.render(<Counter count={-10} />, document.getElementById("app"));
+ReactDOM.render(<Counter />, document.getElementById("app"));
 
 //Run 2 terminals
 //babel src/app.js --out-file=public/scripts/app.js --presets=env,react --watch
