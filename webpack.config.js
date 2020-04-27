269c1c707ed1c4edfaf215aeffedaa48dbdab762
Robin Liu <robinliu106@gmail.com>
2020-04-27 19:00:54 -0400

    install webpack-dev-server, no need to use live-server

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,7 @@ module.exports = {
         ],
     },
     devtool: "cheap-module-eval-source-map", //source map
+    devServer: {
+        contentBase: path.join(__dirname, "public"),
+    },
 };
