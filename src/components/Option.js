23a8236e144070f6b1b97fe8d66aee08f28b3494
Robin Liu <robinliu106@gmail.com>
2020-04-27 22:41:52 -0400

    update components to implicitly return jsx

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,19 +3,17 @@ import React from "react";
 //cannot export default for constants,
 // 1 possible solution is to remove constant definition, but this will cause React to use unknown in dev tools
 //better solution is to name function and export below
-const Option = (props) => {
-    return (
-        <div>
-            {props.optionText}
-            <button
-                onClick={(e) => {
-                    props.handleDeleteOption(props.optionText);
-                }}
-            >
-                Remove
-            </button>
-        </div>
-    );
-};
+const Option = (props) => (
+    <div>
+        {props.optionText}
+        <button
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText);
+            }}
+        >
+            Remove
+        </button>
+    </div>
+);
 
 export default Option;
