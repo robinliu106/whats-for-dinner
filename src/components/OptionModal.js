ba1e48ce87a2f597231ca42a9b56bd09c16d098a
Robin Liu <robinliu106@gmail.com>
2021-02-06 17:10:42 -0500

    edit package

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -9,10 +9,7 @@ const OptionModal = (props) => (
         closeTimeoutMS={200}
         className="modal"
     >
-        <h3 className="modal__title">Selected Option</h3>
-        {props.selectedOption && (
-            <p className="modal__body">{props.selectedOption}</p>
-        )}
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
         <button className="button" onClick={props.handleClearSelectedOption}>
             Okay
         </button>
